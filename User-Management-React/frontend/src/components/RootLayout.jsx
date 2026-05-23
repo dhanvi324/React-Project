40515aeaeb1ad60c96c79f43f96ad7f5b5ea3845
dhanvi324 <dhanvishah2006@gmail.com>
2026-05-23 16:26:01 +0530

    Refactor RootLayout to remove user state handling
    Removed user state management and related functionality.

--- User-Management-React/frontend/src/components/RootLayout.jsx
+++ User-Management-React/frontend/src/components/RootLayout.jsx
@@ -1,19 +1,13 @@
 import { Outlet } from "react-router-dom";
-import { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
 function RootLayout() {
-  const [users, setUsers] = useState([]);
-
-  const addUser = (newUser) => {
-    setUsers((prev) => [...prev, newUser]);
-  };
-
   return (
     <div>
       <Header />
-      <Outlet context={{ users, addUser }} />
+      <Outlet />
+      <Footer />
     </div>
   );
 }
